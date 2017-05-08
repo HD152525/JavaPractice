f3eaa17f8ba7fe4ba552729afe47bf950e53ae09
john <john@happy>
2017-05-08 16:37:28 +0900

    실습과제

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ * 	|_&{type_name}
+ *	
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 8.
+ * </pre>
+ * 
+ * @author       : john
+ * @version		 : 1.0
+ */
+public class PersonTest {
+public static void main(String[] args) {
+	Person p = new Person("Tom");
+	Korean k = new Korean("홍길동");
+	Japanese j = new Japanese("다나카");
+	Chinese c = new Chinese("왕밍");
+	System.out.println(p.toString());
+	System.out.println(k.toString());
+	System.out.println(j.toString());
+	System.out.println(c.toString());
+	System.out.println();
+	p.sayHello();
+	k.sayHello();
+	j.sayHello();
+	c.sayHello();
+	System.out.println();
+	p.sayBye();
+	k.sayBye();
+	j.sayBye();
+	c.sayBye();
+}
+}
